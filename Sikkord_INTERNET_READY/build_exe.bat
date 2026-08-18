730ad9dbbc1ef08c046b59cdda58d4b513edd3e6
mustafacans <mustafacantuna7634@gmail.com>
2026-08-18 19:06:47 +0300

    Delete Sikkord_INTERNET_READY/build_exe.bat

--- Sikkord_INTERNET_READY/build_exe.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title SIKKORD FINAL EXE
-python -m pip install -r requirements.txt
-python -m PyInstaller --noconfirm --clean --onefile --windowed --name Sikkord client.py
-echo.
-echo HAZIR: dist\Sikkord.exe
-pause
